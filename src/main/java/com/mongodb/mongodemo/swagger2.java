21d55549d0cb0469649839d301dd47ce26807687
Gdk666 <763366136@qq.com>
2018-08-09 16:21:10 +0800

    修改

--- src/main/java/com/mongodb/mongodemo/swagger2.java
+++ src/main/java/com/mongodb/mongodemo/swagger2.java
@@ -36,7 +36,7 @@ public class swagger2 {
                 //页面标题
                 .title("Spring Boot 测试使用 Swagger2 构建RESTful API")
                 //创建人
-                .contact(new Contact("MarryFeng", "http://www.baidu.com", ""))
+                .contact(new Contact("Guoz", "http://www.baidu.com", ""))
                 //版本号
                 .version("1.0")
                 //描述
