b97695edc20622e2956cc5e8457351c1f6dd0f77
Gdk666 <763366136@qq.com>
2018-10-17 16:55:41 +0800

    修改SessionUtil

--- guoz-management/src/main/java/Guoz/util/SessionUtil.java
+++ guoz-management/src/main/java/Guoz/util/SessionUtil.java
@@ -2,6 +2,8 @@
 package Guoz.util;
 
 import Guoz.pojo.dto.ManagerDto;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.session.Session;
 
@@ -14,26 +16,28 @@ import org.apache.shiro.session.Session;
 
 public class SessionUtil {
 
-
-     /*用户信息key*/
-
-
+    /**
+     * 用户信息key
+     */
     public static final String USER_SESSION_KEY = "USER_SESSION_KEY";
     public static final String USER_SESSION_LOGOUT_LOGOUT_KEY = "USER_SESSION_LOGOUT_LOGOUT_KEY";
 
-
-
-/**
+    /**
      * 从session中获取当前登录的用户信息
      *
      * @return 用户信息
      */
-
     public static ManagerDto getSession() {
         Session session = SecurityUtils.getSubject().getSession();
-        return (ManagerDto) session.getAttribute(USER_SESSION_KEY);
+        Object user_session = session.getAttribute(USER_SESSION_KEY);
+        JSONObject json = JSONUtil.parseObj(user_session);
+        return JSONUtil.toBean(json, ManagerDto.class);
     }
 
+    /**
+     * 用户信息放入session
+     * @param manager
+     */
     public static void setSession(ManagerDto manager) {
         Session session = SecurityUtils.getSubject().getSession();
         session.setAttribute(USER_SESSION_KEY, manager);
